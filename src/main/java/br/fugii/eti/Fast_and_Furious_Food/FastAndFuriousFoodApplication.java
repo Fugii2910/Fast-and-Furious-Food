0e5d17894cf689184e10d26c7ec8a6961757647c
Fugii <fugiienzo84@gmail.com>
2026-04-08 08:15:55 -0300

    Começo do Projeto

---
+++ src/main/java/br/fugii/eti/Fast_and_Furious_Food/FastAndFuriousFoodApplication.java
@@ -0,0 +1,13 @@
+package br.fugii.eti.Fast_and_Furious_Food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastAndFuriousFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastAndFuriousFoodApplication.class, args);
+	}
+
+}
